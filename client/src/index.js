2bc7fd5f88fe51eef5f4fd74715dc0605eab7ea5
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-07 20:43:24 -0600

    Apollo wrapped App component

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+    uri: "http://localhost:4444/graphql"
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
