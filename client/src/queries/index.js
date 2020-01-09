09c4080643101b4cc2c197f445c86dafcdd725ca
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-08 18:46:56 -0600

    adds successful signinUser mutation

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -1,5 +1,7 @@
 import { gql } from 'apollo-boost';
 
+/*Recipes Queries*/
+
 export const GET_ALL_RECIPES = gql`
 
 query {
@@ -17,4 +19,19 @@ query {
         createdDate
     }
 }
+`;
+
+/*Recipes Mutations*/
+
+/*User Queries*/
+
+/*User Mutations*/
+
+export const SIGNUP_USER = gql`
+mutation ($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+
 `;
--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require('bcrypt');
 
 const createToken = (user, secret, expiresIn) => {
     const { username, email } = user;
@@ -29,6 +29,20 @@ exports.resolvers = {
             }).save();
             return newRecipe;
         },
+        signinUser: async (root, { username, password }, { User }) => {
+            const user = await User.findOne({ username });
+            if(!user) {
+                throw new Error('User not found');
+            }
+            const isValidPassword = await bcrypt.compare(password, user.password);
+            if(!isValidPassword) {
+                throw new Error("Invalid Password")
+            }
+            return { token: createToken(user, process.env.SECRET, "1hr")};
+
+        },
+
+
         signupUser: async (root, { username, email, password }, { User }) => {
             const user = await User.findOne({ username });
             if (user) {
