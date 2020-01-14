d6d07986550826fed6542d4924871b73a9c2bb1f
Joseph Chretien-Golden <joechre@gmail.com>
2020-01-14 14:35:48 -0600

    adds react-pose library

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
+import posed from 'react-pose';
 import { Query } from 'react-apollo';
 import { GET_ALL_RECIPES } from '../queries';
 import RecipeItem from './Recipe/RecipeItem';
+import Spinner from "./Spinner";
 
-const App = () => (
+const RecipeList = posed.ul({
+  shown: {
+    x: "0%",
+    staggerChildren: 100
+  },
+  hidden: {
+    x: "-100%"
+  }
+});
+
+class App extends React.Component {
+  state = {
+    on: false
+  };
+
+  componentDidMount() {
+    setTimeout(this.slideIn, 200);
+  }
+
+  slideIn = () => {
+    this.setState({ on: !this.state.on });
+  };
+
+
+  render() {
+  return (
   <div className="App">
     <h1 className="main-title">
       Find Recipes You <strong>Love</strong>
@@ -15,16 +42,18 @@ const App = () => (
         if(error) return <div>Error</div>
         console.log(data);
       return (
-      <ul className="cards">
+      <RecipeList className="cards">
         {data.getAllRecipes.map(recipe => (
         <RecipeItem key={recipe._id}{...recipe}/>
         ))}
-        </ul>
+        </RecipeList>
         );
       }}
     </Query>
   </div>
 )
+    }
+};
     
 
 export default App;
